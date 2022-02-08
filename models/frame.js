2b4defc41b22c80ede800ef68280b37c8b6f195f
Gregory Oakley-Stevenson <gregory@okonetwork.org.uk>
2022-02-08 22:51:27 +0000

    Fix schema

--- models/frame.js
+++ models/frame.js
@@ -2,6 +2,21 @@
 let mongoose = require("mongoose");
 let Schema = require("mongoose").Schema;
 
+let resultsSchema = new Schema({
+    confidence: {type: mongoose.Decimal128, required: true},
+    plate: {type: String, required: true},
+    patternMatched: {type: Boolean, required: true},
+    coordinates: [{
+        x: {type: mongoose.Decimal128, required: true},
+        y: {type: mongoose.Decimal128, required: true},
+    }],
+    candidates: [{
+        plate: {type: String, required: true},
+        confidence: {type: mongoose.Decimal128, required: true},
+        patternMatched: {type: Boolean, required: true},
+    }],
+});
+
 let frameSchema = new Schema({
     footage: {type: Schema.Types.ObjectId, ref: 'Frame'},
     frameNumber: {type: Number, required: true},
@@ -10,20 +25,7 @@ let frameSchema = new Schema({
     uploadFilename: {type:String,required:true},
     uploaded: {type:Boolean,required:true}, 
     processed: { type: Boolean, default: false },
-    results: {
-        confidence: {type: mongoose.Decimal128, required: true},
-        plate: {type: String, required: true},
-        patternMatched: {type: Boolean, required: true},
-        coordinates: [{
-            x: {type: mongoose.Decimal128, required: true},
-            y: {type: mongoose.Decimal128, required: true},
-        }],
-        candidates: [{
-            plate: {type: String, required: true},
-            confidence: {type: mongoose.Decimal128, required: true},
-            patternMatched: {type: Boolean, required: true},
-        }],
-    }
+    results: [{ type: resultsSchema, default: [] }],
 });
 
 module.exports = frameSchema;
